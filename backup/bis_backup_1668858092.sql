1b14ca512242958880593f7b40c482edf5b20183
wilfredotanuan <119877608+wilfredotanuan@users.noreply.github.com>
2023-01-11 19:58:47 +0800

    Add files via upload

---
+++ backup/bis_backup_1668858092.sql
@@ -0,0 +1,334 @@
+# ABMS : MySQL database backup
+#
+# Generated: Saturday 19. November 2022
+# Hostname: localhost
+# Database: bis
+# --------------------------------------------------------
+
+
+#
+# Delete any existing table `tbl_support`
+#
+
+DROP TABLE IF EXISTS `tbl_support`;
+
+
+#
+# Table structure of table `tbl_support`
+#
+
+
+
+CREATE TABLE `tbl_support` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `email` varchar(60) DEFAULT NULL,
+  `number` varchar(20) DEFAULT NULL,
+  `subject` varchar(100) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+
+
+
+#
+# Delete any existing table `tbl_users`
+#
+
+DROP TABLE IF EXISTS `tbl_users`;
+
+
+#
+# Table structure of table `tbl_users`
+#
+
+
+
+CREATE TABLE `tbl_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `user_type` varchar(20) DEFAULT NULL,
+  `avatar` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO tbl_users VALUES("10","staff","6ccb4b7c39a6e77f76ecfa935a855c6c46ad5611","staff","03052021043218icon.png","2021-05-03 10:32:18");
+INSERT INTO tbl_users VALUES("11","admin","d033e22ae348aeb5660fc2140aec35850c4da997","administrator","09052021074950person.png","2021-05-03 10:33:03");
+
+
+
+#
+# Delete any existing table `tblblotter`
+#
+
+DROP TABLE IF EXISTS `tblblotter`;
+
+
+#
+# Table structure of table `tblblotter`
+#
+
+
+
+CREATE TABLE `tblblotter` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `complainant` varchar(100) DEFAULT NULL,
+  `respondent` varchar(100) DEFAULT NULL,
+  `victim` varchar(100) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `details` varchar(10000) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+
+
+
+
+#
+# Delete any existing table `tblbrgy_info`
+#
+
+DROP TABLE IF EXISTS `tblbrgy_info`;
+
+
+#
+# Table structure of table `tblbrgy_info`
+#
+
+
+
+CREATE TABLE `tblbrgy_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `province` varchar(100) DEFAULT NULL,
+  `town` varchar(100) DEFAULT NULL,
+  `brgy_name` varchar(50) DEFAULT NULL,
+  `number` varchar(50) DEFAULT NULL,
+  `text` text DEFAULT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  `city_logo` varchar(100) DEFAULT NULL,
+  `brgy_logo` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO tblbrgy_info VALUES("1","CAGAYAN","LAL-LO","TUCALANA","0919-1234567","Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Sed porttitor lectus nibh. Curabitur aliquet quam id dui posuere blandit.","09052021075440182970012_615550183178722_2776607156578360582_n.jpg","19112022115223icon.png","19112022122015icon.png");
+
+
+
+#
+# Delete any existing table `tblchairmanship`
+#
+
+DROP TABLE IF EXISTS `tblchairmanship`;
+
+
+#
+# Table structure of table `tblchairmanship`
+#
+
+
+
+CREATE TABLE `tblchairmanship` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO tblchairmanship VALUES("12","Chairman");
+
+
+
+#
+# Delete any existing table `tblofficials`
+#
+
+DROP TABLE IF EXISTS `tblofficials`;
+
+
+#
+# Table structure of table `tblofficials`
+#
+
+
+
+CREATE TABLE `tblofficials` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `chairmanship` varchar(50) DEFAULT NULL,
+  `position` varchar(50) DEFAULT NULL,
+  `termstart` date DEFAULT NULL,
+  `termend` date DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+
+
+
+
+#
+# Delete any existing table `tblpayments`
+#
+
+DROP TABLE IF EXISTS `tblpayments`;
+
+
+#
+# Table structure of table `tblpayments`
+#
+
+
+
+CREATE TABLE `tblpayments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `details` varchar(100) DEFAULT NULL,
+  `amounts` decimal(10,2) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `user` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+
+
+
+
+#
+# Delete any existing table `tblpermit`
+#
+
+DROP TABLE IF EXISTS `tblpermit`;
+
+
+#
+# Table structure of table `tblpermit`
+#
+
+
+
+CREATE TABLE `tblpermit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) DEFAULT NULL,
+  `owner1` varchar(200) DEFAULT NULL,
+  `owner2` varchar(80) DEFAULT NULL,
+  `nature` varchar(220) DEFAULT NULL,
+  `applied` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+
+
+
+
+#
+# Delete any existing table `tblposition`
+#
+
+DROP TABLE IF EXISTS `tblposition`;
+
+
+#
+# Table structure of table `tblposition`
+#
+
+
+
+CREATE TABLE `tblposition` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `position` varchar(50) DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+
+
+
+
+#
+# Delete any existing table `tblprecinct`
+#
+
+DROP TABLE IF EXISTS `tblprecinct`;
+
+
+#
+# Table structure of table `tblprecinct`
+#
+
+
+
+CREATE TABLE `tblprecinct` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `precinct` varchar(100) DEFAULT NULL,
+  `details` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+#
+# Delete any existing table `tblpurok`
+#
+
+DROP TABLE IF EXISTS `tblpurok`;
+
+
+#
+# Table structure of table `tblpurok`
+#
+
+
+
+CREATE TABLE `tblpurok` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `details` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+
+
+
+
+#
+# Delete any existing table `tblresident`
+#
+
+DROP TABLE IF EXISTS `tblresident`;
+
+
+#
+# Table structure of table `tblresident`
+#
+
+
+
+CREATE TABLE `tblresident` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `national_id` varchar(100) DEFAULT NULL,
+  `citizenship` varchar(50) DEFAULT NULL,
+  `picture` text CHARACTER SET utf8mb4 DEFAULT NULL,
+  `firstname` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `middlename` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `lastname` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `alias` varchar(20) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `birthplace` varchar(80) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `civilstatus` varchar(20) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `gender` varchar(20) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `purok` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `voterstatus` varchar(20) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `identified_as` varchar(30) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `phone` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `occupation` varchar(100) DEFAULT NULL,
+  `address` text CHARACTER SET utf8mb4 DEFAULT NULL,
+  `resident_type` int(11) DEFAULT 1,
+  `remarks` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=181 DEFAULT CHARSET=latin1;
+
+
